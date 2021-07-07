4c35091dd0355676d0be97a54605bb90d78b54c7
sirostaib <sirostaib@gmail.com>
2021-07-07 17:23:02 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ build/precache-manifest.0715ecaaf65df212191bbdfb14733a68.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb726784d7c3a5923c8871aeb628cf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f51b12d766df6ce80e",
+    "url": "/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "b3d7f74ba09f03666e1f",
+    "url": "/static/css/main.f77a5406.chunk.css"
+  },
+  {
+    "revision": "89f51b12d766df6ce80e",
+    "url": "/static/js/2.b98f4842.chunk.js"
+  },
+  {
+    "revision": "b3d7f74ba09f03666e1f",
+    "url": "/static/js/main.44e0c1ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49f9e861b38a76c255d9a2e9b02e4c93",
+    "url": "/static/media/468608176-0.49f9e861.jpeg"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/static/media/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/static/media/Roboto-regular.f36638c2.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "ac2b9668b6d7a15581d290cc2e3221b6",
+    "url": "/static/media/asa.ac2b9668.png"
+  },
+  {
+    "revision": "02d679f6bbb95ed518b66421209dfc1c",
+    "url": "/static/media/c5e02fce-c0d2-0d86-c803-4283c6a5fc9f.02d679f6.jpg"
+  },
+  {
+    "revision": "5afe457ceb8f869a89aa637056b236ac",
+    "url": "/static/media/house.5afe457c.png"
+  },
+  {
+    "revision": "15d70e0c2f96658006aa9c474c338c26",
+    "url": "/static/media/verify.15d70e0c.png"
+  }
+]);
